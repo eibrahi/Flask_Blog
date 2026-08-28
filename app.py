6cefc59fc4624829357b07de65d0adc93d50332e
eibrahi <newewi@gmail.com>
2026-08-28 12:35:26 +0200

    Implemented function update and delete 2!

--- app.py
+++ app.py
@@ -16,10 +16,51 @@ def index():
 def add():
     if request.method == 'POST':
         # fill this in the next step
+        new_author = request.form.get('author', '')
+        new_title = request.form.get('title', '')
+        new_content = request.form.get('content', '')
+
+        if blog_posts:
+            new_id = max(post['id'] for post in blog_posts) + 1
+        else:
+            new_id = 1
+
+        new_post = {
+            "id": new_id,
+            "author": new_author,
+            "title": new_title,
+            "content": new_content
+        }
+        blog_posts.append(new_post)
 
         return redirect(url_for('index'))
 
     return render_template('add.html')
+
+@app.route('/delete/<int:post_id>', methods=['GET', 'POST'])
+def delete(post_id):
+    for post in blog_posts:
+        if post['id'] == post_id:
+            blog_posts.remove(post)
+            break
+
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    if request.method == 'GET':
+        for post in blog_posts:
+            if post['id'] == post_id:
+                return render_template('update.html', post=post)
+
+    if request.method == 'POST':
+        for post in blog_posts:
+            if post['id'] == post_id:
+                post['title'] = request.form['title']
+                post['content'] = request.form['content']
+                return redirect(url_for('index'))
+
+    return "Post not found", 404
 
 
 if __name__ == '__main__':
