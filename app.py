546c8969d7564e1fb111b4db846ac5e3a93fd871
eibrahi <newewi@gmail.com>
2026-08-28 09:38:26 +0200

    Adding some html templates

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-blogbeitraege = [
+blog_posts = [
     {"id": 1, "author": "John Doe", "title": "First Post", "content": "This is my first post."},
     {"id": 2, "author": "Jane Doe", "title": "Second Post", "content": "This is another post."}
     # More blog posts can go here...
 ]
+@app.route("/")
+def index():
+    # code to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # fill this in the next step
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
